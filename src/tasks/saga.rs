02cebb645257749649be9814e5915a5986094c8f
Bernd-L <git@bernd.pw>
2020-08-11 19:36:04 +0200

    More work

--- src/tasks/saga.rs
+++ src/tasks/saga.rs
@@ -1,8 +1,32 @@
+use crate::tasks::instruction::Instruction;
+
 #[derive(Debug)]
-pub struct Saga {}
+pub struct Saga {
+  instructions: Vec<Instruction>,
+  state: State,
+  name: String,
+}
 
 impl Saga {
-  pub fn new() -> Saga {
-    Saga {}
+  pub fn with_name(name: String) -> Saga {
+    Saga {
+      name,
+      instructions: Vec::new(),
+      state: State::Pending,
+    }
   }
+
+  pub fn add_instruction(&mut self, instruction: Instruction) -> &mut Saga {
+    self.instructions.push(instruction);
+    self
+  }
+}
+
+#[derive(Debug)]
+enum State {
+  Pending,
+  Running(usize),
+  Aborting(usize),
+  Finished,
+  Aborted,
 }
