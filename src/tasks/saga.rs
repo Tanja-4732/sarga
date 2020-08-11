a9e099b8f196ee93a062b73be8fde513cab06fd3
Bernd-L <git@bernd.pw>
2020-08-11 19:59:19 +0200

    Better builder

--- src/tasks/saga.rs
+++ src/tasks/saga.rs
@@ -1,27 +1,39 @@
 use crate::tasks::instruction::Instruction;
 
+/// A saga is a list of instructions, leading to eventual consistency
 #[derive(Debug)]
 pub struct Saga {
   instructions: Vec<Instruction>,
   state: State,
-  name: String,
+  abort_type: AbortType,
 }
 
 impl Saga {
-  pub fn with_name(name: String) -> Saga {
+  /// Create a new saga with a given name
+  pub fn new(abort_type: AbortType) -> Saga {
     Saga {
-      name,
       instructions: Vec::new(),
       state: State::Pending,
+      abort_type,
     }
   }
 
-  pub fn add_instruction(&mut self, instruction: Instruction) -> &mut Saga {
+  /// Appends an instruction to the end of the saga
+  pub fn add_instruction(mut self, instruction: Instruction) -> Self {
     self.instructions.push(instruction);
     self
   }
+
+  /// Starts the saga in a blocking fashion
+  pub fn start_blocking(&mut self) {
+    // TODO start all instructions in a loop
+    // TODO Set the state inside of the loop
+    self.state = State::Running(1);
+    // TODO Return something
+  }
 }
 
+/// The current state of the saga
 #[derive(Debug)]
 enum State {
   Pending,
@@ -29,4 +41,11 @@ enum State {
   Aborting(usize),
   Finished,
   Aborted,
+}
+
+/// The way the saga should be aborted, if an instruction fails
+#[derive(Debug)]
+pub enum AbortType {
+  Forward,
+  Backward,
 }
--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,7 +1,11 @@
+use crate::tasks::instruction::Instruction;
+use crate::tasks::saga::AbortType;
 use crate::tasks::saga::Saga;
 
 pub fn test_main() {
-  let saga = Saga::new();
+  let mut saga = Saga::new(AbortType::Backward).add_instruction(Instruction {});
+
+  saga.add_instruction(Instruction {});
 
   println!("{:?}", saga);
 }
