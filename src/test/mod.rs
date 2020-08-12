a25096131e41746cc44bcbf4d4a9dd658549aa27
Bernd <git@bernd.pw>
2020-08-12 08:50:04 +0200

    Fixed test/mod.rs

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,13 +1,15 @@
+use crate::tasks::compensation::Compensation;
 use crate::tasks::instruction::Instruction;
 use crate::tasks::saga::AbortType;
 use crate::tasks::saga::Saga;
+use crate::tasks::transaction::Transaction;
 
 pub fn test_main() {
   let saga = Saga::new(AbortType::Backward)
-    .add_instruction(Instruction {})
-    .add_instruction(Instruction {})
-    .add_instruction(Instruction {})
-    .add_instruction(Instruction {});
+    .add_instruction(Instruction::new(Transaction {}, Compensation {}))
+    .add_instruction(Instruction::new(Transaction {}, Compensation {}))
+    .add_instruction(Instruction::new(Transaction {}, Compensation {}))
+    .add_instruction(Instruction::new(Transaction {}, Compensation {}));
 
   println!("{:?}", saga);
 }
